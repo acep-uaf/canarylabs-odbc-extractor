9b0c0072f4477793eebb35471471eb509f759faf
tcmorgan2 <tcmorgan2@alaska.edu>
2019-07-31 16:47:05 -0800

    fixed list error in dictionary writing

--- process_historic_canary.py
+++ process_historic_canary.py
@@ -9,28 +9,6 @@ output as csv containing a single day of data for all parameters'''
 datafolder = 'HistoricData2018'
 dateformat = "%Y-%m-%d %H:%M:%S.%f"
 
-#The historian requires filters
-#for instance 'select * from data' results in fewer records that 'select * from data where time_stamp < 2019-01-01'
-#so we run through day by day
-d1 = datetime.datetime.strptime('2018-01-01 00:00:00.1',"%Y-%m-%d %H:%M:%S.%f")
-dend = datetime.datetime.strptime('2019-01-01 00:00:01.0',"%Y-%m-%d %H:%M:%S.%f")
-d2 = d1 + datetime.timedelta(hours=24)
-
-#The connnection
-cnxn = pyodbc.connect('DSN=CanaryODBCClient;')
-cursor = cnxn.cursor()
-#extract and write data for each day 
-while d2 < dend:
-    #data as list of tuples
-    records = cursor.execute("SELECT * from data WHERE time_stamp > ? AND time_stamp < ? and tag_name like 'ACEP-WS-DWINBOX.Cordova%'",[d1.strftime(dateformat),d2.strftime(dateformat)]).fetchall()
-    #tuples are organized as ('tag_name','description','time_stamp','value','quality')
-    dayDict = putInDictionary(records)
-    writeDictionary(dayDict,(d1.strftime(dateformat)).split(' ')[0])
-    d2 = d1 + datetime.timedelta(hours=24)
-#close database connection
-cnxn.close()
-
-
 def putInDictionary(records):
     '''Put a list of tuples into a dictionary. Each time_stamp gets its own row and all tags get put in their corresponding time_stamped row.
     records is a list of tuples organized as ('tag_name','description','time_stamp','value','quality')
@@ -39,7 +17,7 @@ def putInDictionary(records):
     tags = set()
     for row in records:
         tags.add(row[1])
-        time_stamp = row[2]
+        time_stamp = row[2].strftime(dateformat)
         tag = row[1]    
         rowdata = {'time_stamp':time_stamp,tag + '_value':row[3], tag +'_quality': row[4]}
         if time_stamp in fileDict.keys():
@@ -49,6 +27,7 @@ def putInDictionary(records):
     headers = [t + '_quality' for t in tags] + [t + '_value' for t in tags]
     headers.sort()
     fileDict['headers'] = ['time_stamp'] + headers
+    return fileDict
     
 def writeDictionary(dayDict,fileTag):
     ''' write a dictionary of data rows to a csv file
@@ -61,8 +40,38 @@ def writeDictionary(dayDict,fileTag):
         csv_writer = csv.DictWriter(Mycsv,fieldnames=headers)
         csv_writer.writeheader()
         for r in dayDict.keys(): #r will be the rows timestamp
-            csv_writer.writerow(dayDict[r])
+            if r != 'headers':
+                csv_writer.writerow(dayDict[r])
            
+
+#The historian requires filters
+#for instance 'select * from data' results in fewer records that 'select * from data where time_stamp < 2019-01-01'
+#so we run through day by day
+d1 = datetime.datetime.strptime('2018-01-01 00:00:00.1',"%Y-%m-%d %H:%M:%S.%f")
+dend = datetime.datetime.strptime('2019-05-23 00:00:01.0',"%Y-%m-%d %H:%M:%S.%f")
+d2 = d1 + datetime.timedelta(hours=24)
+
+#The connnection
+cnxn = pyodbc.connect('DSN=CanaryODBCClient;')
+cursor = cnxn.cursor()
+#extract and write data for each day 
+while d2 < dend:
+    try:
+        #data as list of tuples
+        records = cursor.execute("SELECT * from data WHERE time_stamp < '" + d2.strftime(dateformat) + "' and tag_name like 'ACEP-WS-DWINBOX.Cordova%'").fetchall()
+        #tuples are organized as ('tag_name','description','time_stamp','value','quality')
+        dayDict = putInDictionary(records)
+        if dayDict != None:
+            writeDictionary(dayDict,(d2.strftime(dateformat)).split(' ')[0])
+    except Exception as e:
+        print(d2)
+        print(e)
+    finally:
+        d2 = d2 + datetime.timedelta(hours=24)
+#close database connection
+cnxn.close()
+
+
 
     
     
