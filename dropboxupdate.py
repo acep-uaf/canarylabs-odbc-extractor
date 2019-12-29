d7f3105934f1b77386d87b122e41eaa0f8ba2a2b
tcmorgan2 <tcmorgan2@alaska.edu>
2019-12-29 10:12:02 -0900

    added dropbox update to export process

---
+++ dropboxupdate.py
@@ -0,0 +1,85 @@
+"""Syncrhonize D:HistorianData folder with dropbox.
+"""
+
+import asyncio
+import urllib
+import dropbox
+import os
+import sys
+
+from dropbox.exceptions import AuthError
+
+DROPBOX_URL = "https://www.dropbox.com/scl/fo/pjgnsgq98aw1xoan22imd/AAAXO5qNZT9NZN4h7ILsOSp7a/Histlog?dl=0"
+DATAFOLDER = "C:\\RADIANCE\\HISTORIAN"
+
+def ConfigObject(config_path):
+    "read a configuration file to retrieve access token"
+    configDict = {}
+    with open(config_path,'r') as config:
+        for line in config.readlines():
+            try:
+                configDict[line.split("=")[0]] = line.split("=")[1].rstrip()
+            except:
+                pass
+    return configDict
+
+
+def getFileList(folder):
+    "list the files we already have - won't overwrite these files"
+    try:
+        return os.listdir(folder)
+    except:
+        return []
+
+async def processEntries(dbx,url,currentFiles, entries):
+    """
+
+    :param dbx: dropbox connection
+    :param url: url of the folder containing new files
+    :param currentFiles: files already stored locally
+    :param entries: filemetadata returned from list_files on linked folder
+    :return: None, writes file to data folder
+    """
+    for fm in entries:
+        if fm.name not in currentFiles:
+            dlink = dbx.sharing_get_shared_link_file(url, "/" + fm.name)
+            fileurl = dlink[0].url.replace("dl=0", "dl=1")
+            u = await urllib.request.urlopen(fileurl)
+            data = u.read()
+            u.close()
+            with open(fm.name, "wb") as f:
+                f.write(data)
+
+async def download(dbx):
+    """
+    lists all files contained in linked folder and writes any new files to the local data folder
+    :param dbx: dropbox connection
+    :return: None, files written to data folder
+    """
+    currentFiles = getFileList(DATAFOLDER)
+    shared_link = dropbox.files.SharedLink(url=DROPBOX_URL)
+    files =  dbx.files_list_folder(path="", shared_link=shared_link)
+    await processEntries(dbx, DROPBOX_URL, currentFiles, files.entries)
+    while files.has_more:
+        files = dbx.files_list_folder_continue(files.cursor)
+        await processEntries(dbx,DROPBOX_URL,currentFiles, files.entries)
+
+
+def main():
+    """download the contents of a given linked dropbox folder"""
+    path = os.path.dirname(__file__)
+    config = ConfigObject(os.path.join(path,'config.ini')) #read config file with access token
+    dbx = dropbox.Dropbox(config['access_token'])
+    #make sure we connected
+    try:
+        dbx.users_get_current_account()
+    except AuthError:
+        sys.exit("ERROR: Invalid access token; try re-generating an "
+                 "access token from the app console on the web.")
+    #download the contents
+    download(dbx)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- process_historic_canary.py
+++ process_historic_canary.py
@@ -1,72 +1,31 @@
 import pyodbc
 import csv
 import os
-import datetime 
+import datetime
+from zipfile import ZipFile
+import dropboxupdate
 
 '''script to read data out of canary labs historian and create a crosstab
 output as csv containing a single day of data for all parameters'''
 #CONSTANTS
-DATAFOLDER = 'PowerOutage20190814'
+DATAFOLDER = 'HistoricalData2018'
 DATEFORMAT = "%Y-%m-%d %H:%M:%S.%f"
-RAW = os.path.join(DATAFOLDER,'rawData')
-COMBINED = os.path.join(DATAFOLDER,'CombinedChannels')
 BYCHANNEL = os.path.join(DATAFOLDER,'ByChannel')
 
-def putInDictionary(records):
-    '''Put a list of tuples into a dictionary. Each time_stamp gets its own row and all tags get put in their corresponding time_stamped row.
-    records is a list of tuples organized as ('tag_name','description','time_stamp','value','quality')
-    '''
-    fileDict={}
-    channelDict={} #a dictionary of dictionaries. Each key is a channel name
-    tags = set()
-    for row in records:
-        tags.add(row[1])
-        time_stamp = row[2].strftime(DATEFORMAT)
-        tag = row[1]  #tag is the name of the channel
-        rowdata = {'time_stamp':time_stamp,tag + '_value':row[3], tag +'_quality': row[4]}
-        channelData = {'time_stamp':time_stamp,'value':row[3], 'quality': row[4]}
-        if tag in channelDict.keys():
-            channelDict[tag][time_stamp] = channelData
-           
-        else:
-            tagDict = {}
-            tagDict['headers']=['time_stamp','value','quality']
-            tagDict[time_stamp] = channelData
-            channelDict[tag] = tagDict
-                       
-        if time_stamp in fileDict.keys():
-            fileDict[time_stamp].update(rowdata) #add the new tag values to the row
-        else:
-            fileDict[time_stamp] = rowdata
-    headers = [t + '_quality' for t in tags] + [t + '_value' for t in tags]
-    headers.sort()
-    fileDict['headers'] = ['time_stamp'] + headers
-    return fileDict,channelDict
-
-
-def writeDictionary(dayDict,filePath):
-    ''' write a dictionary of data rows to a csv file
-    filePath is the path to the csv file including file name and extension
-    dayDict is a dictionary of data rows with a key named 'header' that contains column names
-    '''
-    headers = dayDict['headers']
-    
-    with open(filePath, mode='w',newline='') as Mycsv:
-        csv_writer = csv.DictWriter(Mycsv,fieldnames=headers)
-        csv_writer.writeheader()
-        for r in dayDict.keys(): #r will be the rows timestamp
-            if r != 'headers':
-                csv_writer.writerow(dayDict[r])
-          
+       
 def writeRecords(records,filepath):
     '''write a csv file of raw table data extracted from the Historian data table'''
-    
-    header = ['tag_name','description','time_stamp','value','quality']
-    with open(filepath, mode='w',newline='') as Mycsv:
+    header = []
+
+    if not os.path.exists(filepath):
+        header = ['tag_name','description','time_stamp','value','quality']
+    with open(filepath, mode='a',newline='') as Mycsv:
         csv_writer = csv.writer(Mycsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(header)
-        for row in records:
-            csv_writer.writerow(row)
+        if len(header) > 0:
+            csv_writer.writerow(header)
+    
+        [csv_writer.writerow(row) for row in records]
+        
 
 def checkCreatePath(paths):
     '''checks for the presense of individual folders in a list of file paths and creates them
@@ -79,63 +38,94 @@ def checkCreatePath(paths):
         paths.pop(0)
         return checkCreatePath(paths)
 
-def writeChannelDictionary(channelDict, filePrefix):
-  
-    for d in channelDict.keys():
-        if d != 'headers':
-            writeDictionary(channelDict[d],filePrefix + d + '.csv') #each key is a channel
-        
 
+years = [2018,2019]
+months = months = list(range(1,13,1))
+def zipByMonth(myDirectory):
+    files = os.listdir(myDirectory)
+    os.chdir(myDirectory)
+    def fillZip(f,zipObj):
+        s = zipObj.write(f)
+        os.remove(f)
+    for y in years:
+        for m in months:
+            matchString = str(y)+ "-" + '{:02d}'.format(m)
+            zipFiles = [f for f in files if matchString in f]
+            if len(zipFiles) > 0:
+                with ZipFile(matchString + ".zip", 'w') as zipObj:
+                    [fillZip(f,zipObj) for f in zipFiles]
                 
-#The historian requires filters
-#for instance 'select * from data' results in fewer records that 'select * from data where time_stamp < 2019-01-01'
+#The historian requires datetime filters
+#for instance 'select * from data' returns only records from the most recent hour 
 #so we run through day by day
-d1 = datetime.datetime.strptime('2019-08-13 00:00:00.100000',"%Y-%m-%d %H:%M:%S.%f")
-dend = datetime.datetime.strptime('2019-08-17 00:00:00.100000',"%Y-%m-%d %H:%M:%S.%f")
-d2 = d1 + datetime.timedelta(hours=24)
+dstart = datetime.datetime.strptime('2018-01-01 00:00:00.100000',"%Y-%m-%d %H:%M:%S.%f")
+dend = datetime.datetime.strptime('2019-01-01 00:00:00.100000',"%Y-%m-%d %H:%M:%S.%f")
 
+    
 #The connnection
-cnxn = pyodbc.connect('DSN=CanaryODBCClient;')
-cursor = cnxn.cursor()
-
-#make sure data folders exist, or; create them
-checkCreatePath([RAW,BYCHANNEL,COMBINED])
-
-#extract and write data for each day 
-while d2 < dend:
+def processData(d1,dend,tries = 0):
+    logFile = open(os.path.join(*[BYCHANNEL,datetime.datetime.now().strftime("%Y-%m-%d %H%M%S")+'log.txt']),"w+")
+    pyodbc.pooling = False
     try:
-        #data as list of tuples
-        records = cursor.execute("SELECT * from data WHERE time_stamp < '" + d2.strftime(DATEFORMAT) + "' and tag_name like 'ACEP-WS-DWINBOX.Cordova%'").fetchall()
-        #records = cursor.execute("SELECT * from data WHERE time_stamp < '2019-07-30 00:00:00.100000'").fetchall()
-        #tuples are organized as ('tag_name','description','time_stamp','value','quality')
-        newcsvName = ((d2 - datetime.timedelta(hours=24)).strftime(DATEFORMAT)).split(' ')[0] + 'rawData.csv'
-        filepath = os.path.join(*[RAW,newcsvName])
-        
-        #write the raw data
-        writeRecords(records,filepath)
-        #convert data to dictionaries for re-formatting csv's
-        dayDict,channelDict = putInDictionary(records)
-        
-        #write the combined csv
-        if dayDict != None:
-            newcsvName = ((d2 - datetime.timedelta(hours=24)).strftime(DATEFORMAT)).split(' ')[0] + 'allChannels.csv'
-            filepath = os.path.join(*[COMBINED,newcsvName])
-            writeDictionary(dayDict,filepath)
-            
-        #write the individual channels to csv
-        if channelDict != None:
-            newcsvName = ((d2 - datetime.timedelta(hours=24)).strftime(DATEFORMAT)).split(' ')[0]
-            filepath = os.path.join(*[BYCHANNEL,newcsvName]) #this is only the file prefix
-            writeChannelDictionary(channelDict,filepath)
-    except Exception as e:
-        print(d2)
-        print(e)
-    finally:
-        d2 = d2 + datetime.timedelta(hours=24)
-#close database connection
-cnxn.close()
+        cnxn = pyodbc.connect('DSN=CanaryODBCClient;')
+        cursor = cnxn.cursor()
+        tags = cursor.execute("SELECT * FROM tags WHERE tag_name like '%.Cordova.%'").fetchall()
+        for t in tags:
+            print(t[0])
+            d1 = dstart
+            d2 = d1 + datetime.timedelta(days=1)
+            #extract and write data for each day 
+            while d2 < dend:
+                
+                try:          
+                    #data as list of tuples
+                    records= cursor.execute("SELECT tag_name, description, time_stamp, value, quality from data WHERE time_stamp > '" + d1.strftime(DATEFORMAT) + "'AND time_stamp < '" + d2.strftime(DATEFORMAT) + "' and tag_name = '" + t[0] +"'").fetchall()
+                    #tuples are organized as ('tag_name','description','time_stamp','value','quality')
+                    newcsvName = t[0].replace(".","_") + (d1.strftime(DATEFORMAT)).split(' ')[0] +".csv"
+                    filepath = os.path.join(*[BYCHANNEL,newcsvName]) #this is only the file prefix
+                    if len(records) > 0:
+                        writeRecords(records,filepath)             
 
+                    
+                except pyodbc.Error as er:
+                    logFile.write("pyodbc error for channel {0} on {1} \n".format(t[0],d2))
+                    logFile.write(str(er) +"/n")
+                   
+                    cursor.cancel()
+                    if 'retrieving data for column' in er.args[1]:
+                        logFile.write("Moving to next date value\n")
+                        
+                        pass
+                    else:
+                         tries = tries +1
+                         
+                         if tries < 10:
+                            pass
+                         else:
+                            cnxn.close()
+                            logFile.write("connections closed\n")
+                            try:
+                                cnxn = pyodbc.connect('DSN=CanaryODBCClient;')
+                                cursor = cnxn.cursor()
+                            except: 
+                                raise
+                except Exception as e:
+                    logFile.write("Exception for channel {0} on {1}".format(t[0],d2))
+                    logFile.write(str(e))
+                    
+                d1 = d1 + datetime.timedelta(days=1)#bump up d1
+                d2 = d1 + datetime.timedelta(days=1)#bump up d2
+                                                  
+    except:
+        raise
+    finally:      
+        logFile.close()
+        cnxn.close()
 
+def main():
+    dropboxupdate.main()
+    processData(dstart,dend)
+    zipByMonth(BYCHANNEL)
 
-    
-    
+if __name__ == '__main__':
+    main()
